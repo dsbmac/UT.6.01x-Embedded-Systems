b3d0e5afed81249845180de766348681d94a84b8
dsbmac <dsbmach@gmail.com>
2014-03-03 23:33:51 -0500

    Lab7 initialization complete grader 50pc

--- Lab7_SOS/main.c
+++ Lab7_SOS/main.c
@@ -2,8 +2,8 @@
 // Lab7_SOS, main.c
 
 // Runs on LM4F120 or TM4C123 LaunchPad
-// Input from PF4(SW1) and PF0(SW2), output to PF3 (Green LED)
-// Pressing SW1 starts SOS (Green LED flashes SOS).
+// Input from PF4(SW1) and PF0(SW2), output to PF3 PF2 PF1 (Yellow LED)
+// Pressing SW1 starts SOS (Yellow LED flashes SOS).
 //    S: Toggle light 3 times with 1/2 sec gap between ON....1/2sec....OFF
 //    O: Toggle light 3 times with 2 sec gap between ON....2sec....OFF
 //    S: Toggle light 3 times with 1/2 sec gap between ON....1/2sec....OFF
@@ -32,6 +32,10 @@
 //   Global Variables
 unsigned long SW1; // input from PF4
 unsigned long SW2; // input from PF0
+unsigned long Out1; // output PF1
+unsigned long Out2; // output PF2
+unsigned long Out4; // output PF4
+
 //   Function Prototypes
 void PortF_Init(void);
 void FlashSOS(void);
@@ -39,6 +43,20 @@ void delay(unsigned long halfsecs);
 void EnableInterrupts(void);  // Enable interrupts
 
 // 3. Subroutines Section
+
+/* 
+pseudo code
+1) Make PF1, PF2, and PF3 outputs.
+     Make PF0 and PF4 inputs (enable PUR for PF0 and PF4).
+2) If either SW1 or SW2 are off, the LEDs should be off.
+    If both SW1 and SW2 are on, the SOS is sent on the yellow LED
+            a) Send an ‘S’ as short short short pulses on the yellow LED
+            b) Send an ‘O’ as long long long pulses on the yellow LED
+            c) Send an ‘S’ as short short short pulses on the yellow LED
+            d) Wait 4 seconds
+3) Repeat step 2 over and over.
+*/
+
 // MAIN: Mandatory for a C Program to be executable
 int main(void){
   TExaS_Init(SW_PIN_PF40, LED_PIN_PF321);  // activate grader and set system clock to 80 MHz
@@ -47,18 +65,23 @@ int main(void){
   while(1){
     do{
       SW1 = GPIO_PORTF_DATA_R&0x10; // PF4 into SW1
-    }while(SW1 == 0x10);
+    } while(SW1 == 0x10);
     do{
-      FlashSOS();
 		  SW2 = GPIO_PORTF_DATA_R&0x01; // PF0 into SW2
-		}while(SW2 == 0x01);
+		} while(SW2 == 0x01);
+		if (SW1 == 0X10 & SW2 == 0X01) {
+			FlashSOS();
+			delay(1); // delay between signals
+		}
   }
 }
+
 // Subroutine to initialize port F pins for input and output
 // PF4 is input SW1 and PF2 is output Blue LED
 // Inputs: None
 // Outputs: None
 // Notes: ...
+
 void PortF_Init(void){ volatile unsigned long delay;
   SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
   delay = SYSCTL_RCGC2_R;           // delay   
@@ -67,11 +90,12 @@ void PortF_Init(void){ volatile unsigned long delay;
   GPIO_PORTF_AMSEL_R &= 0x00;        // 3) disable analog function
   GPIO_PORTF_PCTL_R &= 0x00000000;   // 4) GPIO clear bit PCTL  
   GPIO_PORTF_DIR_R &= ~0x11;          // 5.1) PF4,PF0 input, 
-  GPIO_PORTF_DIR_R |= 0x08;          // 5.2) PF3 output  
+  GPIO_PORTF_DIR_R |= 0x0E;          // 5.2) PF3 PF2 PF1 output  
   GPIO_PORTF_AFSEL_R &= 0x00;        // 6) no alternate function
   GPIO_PORTF_PUR_R |= 0x11;          // enable pullup resistors on PF4,PF0       
   GPIO_PORTF_DEN_R |= 0x1F;          // 7) enable digital pins PF4-PF0     
 }
+
 // Color    LED(s) PortF
 // dark     ---    0
 // red      R--    0x02
@@ -81,8 +105,8 @@ void PortF_Init(void){ volatile unsigned long delay;
 // sky blue -GB    0x0C
 // white    RGB    0x0E
 
-// Subroutine to Flash a green LED SOS once
-// PF3 is green LED: SOS 
+// Subroutine to Flash a yellow LED SOS once
+// (PF3 high, PF2 low, and PF1 high) is green LED: SOS 
 //    S: Toggle light 3 times with 1/2 sec gap between ON....1/2sec....OFF
 //    O: Toggle light 3 times with 2 sec gap between ON....2sec....OFF
 //    S: Toggle light 3 times with 1/2 sec gap between ON....1/2sec....OFF
@@ -91,28 +115,27 @@ void PortF_Init(void){ volatile unsigned long delay;
 // Notes: ...
 void FlashSOS(void){
   //S
-  GPIO_PORTF_DATA_R |= 0x08;  delay(1);
-  GPIO_PORTF_DATA_R &= ~0x08; delay(1);
-  GPIO_PORTF_DATA_R |= 0x08;  delay(1);
-  GPIO_PORTF_DATA_R &= ~0x08; delay(1);
-  GPIO_PORTF_DATA_R |= 0x08;  delay(1);
-  GPIO_PORTF_DATA_R &= ~0x08; delay(1);
+  GPIO_PORTF_DATA_R |= 0x0A;  delay(1);
+  GPIO_PORTF_DATA_R &= ~0x0A; delay(1);
+  GPIO_PORTF_DATA_R |= 0x0A;  delay(1);
+  GPIO_PORTF_DATA_R &= ~0x0A; delay(1);
+  GPIO_PORTF_DATA_R |= 0x0A;  delay(1);
+  GPIO_PORTF_DATA_R &= ~0x0A; delay(1);
   //O
-  GPIO_PORTF_DATA_R |= 0x08; delay(4);
-  GPIO_PORTF_DATA_R &= ~0x08;delay(4);
-  GPIO_PORTF_DATA_R |= 0x08; delay(4);
-  GPIO_PORTF_DATA_R &= ~0x08;delay(4);
-  GPIO_PORTF_DATA_R |= 0x08; delay(4);
-  GPIO_PORTF_DATA_R &= ~0x08;delay(4);
+  GPIO_PORTF_DATA_R |= 0x0A; delay(4);
+  GPIO_PORTF_DATA_R &= ~0x0A;delay(4);
+  GPIO_PORTF_DATA_R |= 0x0A; delay(4);
+  GPIO_PORTF_DATA_R &= ~0x0A;delay(4);
+  GPIO_PORTF_DATA_R |= 0x0A; delay(4);
+  GPIO_PORTF_DATA_R &= ~0x0A;delay(4);
   //S
-  GPIO_PORTF_DATA_R |= 0x08; delay(1);
-  GPIO_PORTF_DATA_R &= ~0x08;delay(1);
-  GPIO_PORTF_DATA_R |= 0x08; delay(1);
-  GPIO_PORTF_DATA_R &= ~0x08;delay(1);
-  GPIO_PORTF_DATA_R |= 0x08; delay(1);
-  GPIO_PORTF_DATA_R &= ~0x08;delay(1);
+  GPIO_PORTF_DATA_R |= 0x0A; delay(1);
+  GPIO_PORTF_DATA_R &= ~0x0A;delay(1);
+  GPIO_PORTF_DATA_R |= 0x0A; delay(1);
+  GPIO_PORTF_DATA_R &= ~0x0A;delay(1);
+  GPIO_PORTF_DATA_R |= 0x0A; delay(1);
+  GPIO_PORTF_DATA_R &= ~0x0A;delay(1);
   delay(10); // Delay for 5 secs in between flashes 
-
 }
 
 // Subroutine to delay in units of half seconds
@@ -124,7 +147,7 @@ void FlashSOS(void){
 void delay(unsigned long time){
   unsigned long i;
   while(time > 0){
-    i = 6666665;
+    i = 5333332;
     while(i > 0){
       i = i - 1;
     }
